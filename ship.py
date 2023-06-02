bab393da1ca41e67cb086aef7ef62cd03299dc5d
SebTheRed <seb.belfonti@gmail.com>
2023-06-02 17:45:34 -0400

    Assignment #2

---
+++ ship.py
@@ -0,0 +1,29 @@
+#Sebastian Belfonti
+#COSC 110-821
+#This class instantializes with a user input of weight.
+#The weight is then assigned to a variable that is then passed through a series of if statements.
+#It's important to start with the highest weight to ensure the logic is passed down correctly.
+#The string `self.returnString` is concatenated with the value of the appropriate shipping cost as a string.
+#That string is then returned to the user.
+
+#P.S. Please let me know if the color codes are unacceptable.
+
+class ShippingCharges:
+    def __init__ (self, weight):
+        self.packageWeight = weight
+        self.returnString = "\033[33mIt will cost you: "
+        if self.packageWeight > 10:
+            self.returnString += "\033[32m$4.74"
+        elif self.packageWeight > 6:
+            self.returnString += "\033[32m$4.00"
+        elif self.packageWeight > 2:
+            self.returnString += "\033[32m$3.00"
+        else:
+            self.returnString += "\033[32m$1.50"
+        self.returnString += "\033[33m to ship this package!\033[0m"
+        print(self.returnString)
+
+weightInput = float(input("Please enter the weight of what you are trying to ship: "))
+shippingInstance = ShippingCharges(weightInput)
+            
+        
---
+++ weight.py
@@ -0,0 +1,20 @@
+#Sebastian Belfonti
+#COSC 110-821
+#This class is instantialized with an input (as a float) from the user.
+#This input is then multiplied by 9.8 to create the weight aka Newtons.
+#The user is then alerted whether or not the weight is too heavy, too light, or just right.
+
+class Weight:
+    def __init__ (self, mass):
+        self.weightInNewtons = mass * 9.8
+    def calcWeight (self):
+        if self.weightInNewtons > 500:
+            print("Too Heavy!")
+        elif self.weightInNewtons < 100:
+            print("Too Light!")
+        else:
+            print("Weight is acceptable!")
+
+inputMass = float(input("Please enter the mass of the object: "))
+weightInstance = Weight(inputMass)
+weightInstance.calcWeight()
